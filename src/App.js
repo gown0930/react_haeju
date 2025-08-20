995f8542fc063573fa1953efd69f251d628c107e
haeju <haeju0930@gmail.com>
2025-08-21 00:57:52 +0900

    react_Create

--- src/App.js
+++ src/App.js
@@ -34,6 +34,24 @@ function Article(props){
       </article>
 }
 
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(event)=>{
+      event.preventDefault();
+      const title =event.target.title.value
+      const body =event.target.body.value
+      //이벤트가 발생한 태그 = form 태그
+
+      props.onCreate(title,body)
+    }}>
+      <p><input type="text" name="title" placeholder='title'/></p>
+      <p><textarea name="body" placeholder='body'></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+
 function App() {
   // const _mode = useState('WELCOME')
   // const mode =_mode[0];
@@ -41,11 +59,12 @@ function App() {
   
   const [mode,setMode] =useState('WELCOME')
   const [id, setId] =useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics,setTopics] = useState([
     {id:1, title: 'html', body: 'html is ...'},
     {id:2, title: 'css', body: 'css is ...'},
     {id:3, title: 'javascript', body: 'javascript is ...'}
-  ]
+  ])
   let content =null;
   if(mode ==='WELCOME'){
     content=<Article title="welcome" body="Hello, web"></Article>
@@ -58,6 +77,16 @@ function App() {
       }
     }
     content=<Article title={title} body={body}></Article>
+  }else if(mode ==='CREATE'){
+    content=<Create onCreate={(_title,_body)=>{
+      const newTopic={id:nextId,title:_title,body:_body}
+      const newTopics=[...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ')
+      setId(nextId)
+      setNextId(nextId+1)
+    }}></Create>
   }
   return (
     <div className="App">
@@ -71,6 +100,10 @@ function App() {
       }}></Nav>
 
       {content}
+      <a href="/create" onClick={(event)=>{
+        event.preventDefault();
+        setMode('CREATE')
+      }}>Create</a>
     
     </div>
   );
